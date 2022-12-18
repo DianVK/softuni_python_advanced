3283a7363ff571dadc7d12cf81745d5856dcf7ee
Dian VK <diankinanev@abv.bg>
2022-12-18 11:33:10 +0200

    resolution

--- Advanced/Lists as Stacks and Queues - Exercise/balanced_parentheses.py
+++ Advanced/Lists as Stacks and Queues - Exercise/balanced_parentheses.py
@@ -1,21 +1,19 @@
 from collections import deque
-
-parentheses = deque(input())
-open_per = deque()
-balanced = True
-while parentheses:
-    check_par = parentheses.popleft()
-    if check_par in "{[(":
-        open_per.append(check_par)
-    elif not open_per:
+sequence = deque(input())
+balanced = False
+while sequence:
+    left = sequence.popleft()
+    right = sequence.pop()
+    if left == "(" and right == ")":
+        balanced = True
+    elif left == "{" and right == "}":
+        balanced = True
+    elif left == "[" and right == "]":
+        balanced = True
+    else:
         balanced = False
         break
-    else:
-        if f"{open_per.pop() + check_par}" not in "{}()[]":
-            balanced = False
-            break
-
-if balanced and not open_per:
+if balanced:
     print("YES")
 else:
     print("NO")
