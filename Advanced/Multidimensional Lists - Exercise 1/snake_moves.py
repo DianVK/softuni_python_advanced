2c6544bb02364cd0e5bec3e20a75bc3eb3013248
DianVK <diankinanev@abv.bg>
2022-12-22 12:47:32 +0200

    initial commit

---
+++ Advanced/Multidimensional Lists - Exercise 1/snake_moves.py
@@ -0,0 +1,16 @@
+rows, cols = [int(x) for x in input().split()]
+snake = input()
+
+index_snake = 0
+
+for row in range(rows):
+    result = []
+    for col in range(cols):
+        if index_snake == len(snake):
+            index_snake = 0
+        if row % 2 == 0:
+            result.append(snake[index_snake])
+        else:
+            result.insert(0, snake[index_snake])
+        index_snake += 1
+    print(*result, sep="")
