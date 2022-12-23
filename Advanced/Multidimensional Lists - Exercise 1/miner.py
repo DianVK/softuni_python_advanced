8e8be1877bb5bf2a1d8011b281886ff98321a842
DianVK <diankinanev@abv.bg>
2022-12-23 11:40:04 +0200

    initial commit

--- Advanced/Multidimensional Lists - Exercise 1/miner.py
+++ Advanced/Multidimensional Lists - Exercise 1/miner.py
@@ -1,17 +1,34 @@
-def check_location(row,col):
-    if 0 <= row < matrix_size and 0 <= col < matrix_size:
-        location_symbol = matrix[row][col]
-        print(location_symbol)
+def check_location(r,c):
+    global coals_counter
+    global row
+    global column
+    global matrix
+    global end_reached
+    if 0 <= r < matrix_size and 0 <= c < matrix_size:
+        location_symbol = matrix[r][c]
+        if location_symbol == "c":
+            coals_counter += 1
+            if coals_counter == coals_count:
+                print(f"You collected all coal! ({row}, {column})")
+                exit()
+        elif location_symbol == "e":
+            print(f"Game over! ({row}, {column})")
+            exit()
+        if command == "left" or command == "right" or command == "up" or command == "down":
+            matrix[row][column] = "*"
+            row = r
+            column = c
 
 
 matrix_size = int(input())
 commands = input().split()
 matrix=[[x for x in input().split()] for x in range(matrix_size)]
 
-movement = {"up": [0, -1], "down": [0, 1], "left": [-1, 0], "right": [1, 0]}
+movement = {"up": [-1, 0], "down": [+1, 0], "left": [0, -1], "right": [0, +1]}
 row,column = 0,0
 coals_count = 0
-end_reached = False
+coals_counter = 0
+
 
 for x in range(len(matrix)):
     for y in range(len(matrix[x])):
@@ -20,30 +37,31 @@ for x in range(len(matrix)):
         if matrix[x][y] == "c":
             coals_count += 1
 
-while not end_reached:
-    for command in commands:
-        if command == "left":
-            left_row = int(row)
-            left_column = int(column)
-            left_row += movement['left'][0]
-            left_column += movement['left'][1]
-            check_location(left_row,left_column)
-        if command == "right":
-            left_row = int(row)
-            left_column = int(column)
-            left_row += movement['right'][0]
-            left_column += movement['right'][1]
-            check_location(row,column)
-        if command == "up":
-            left_row = int(row)
-            left_column = int(column)
-            left_row += movement['up'][0]
-            left_column += movement['up'][1]
-            check_location(row,column)
-        if command == "down":
-            left_row = int(row)
-            left_column = int(column)
-            left_row += movement['down'][0]
-            left_column += movement['down'][1]
-            check_location(row,column)
-    print()
+
+for command in commands:
+    left_row = int(row)
+    left_column = int(column)
+    if command == "left":
+        left_row += movement['left'][0]
+        left_column += movement['left'][1]
+        check_location(left_row,left_column)
+    elif command == "right":
+        left_row += movement['right'][0]
+        left_column += movement['right'][1]
+        check_location(left_row,left_column)
+    elif command == "up":
+        left_row += movement['up'][0]
+        left_column += movement['up'][1]
+        check_location(left_row,left_column)
+    elif command == "down":
+        left_row += movement['down'][0]
+        left_column += movement['down'][1]
+        check_location(left_row,left_column)
+
+coals_left = 0
+for x in range(len(matrix)):
+    for y in range(len(matrix[x])):
+        if matrix[row][column] == "c":
+            coals_left += 1
+print(f"{coals_left} pieces of coal left. {(row,column)}")
+
