e9708a2ad6589f73dd614a0d2f1bcb4b92f2ecb1
DianVK <diankinanev@abv.bg>
2022-12-28 13:27:44 +0200

    resolution

--- Advanced/Functions Advanced - Lab/rectangle.py
+++ Advanced/Functions Advanced - Lab/rectangle.py
@@ -1,6 +1,12 @@
 def rectangle(length, width):
     if not all([isinstance(x, int) for x in [length, width]]):
         return "Enter valid values!"
+    #def check_valid(length,width):
+    #check_length,check_width = isinstance(length, int),isinstance(width, int)
+    #if not check_length or not check_width:
+    #    def rectangle(length, width):
+    #        if not all([isinstance(x, int) for x in [length, width]]):
+    #            return "Enter valid values!"
 
     def area():
         return length * width
@@ -9,3 +15,7 @@ def rectangle(length, width):
         return (length + width) * 2
 
     return f"Rectangle area: {area()}\nRectangle perimeter: {perimeter()}"
+
+
+print(rectangle(2, 10))
+print(rectangle('2', 10))
