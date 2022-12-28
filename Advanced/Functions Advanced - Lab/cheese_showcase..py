610f46138a25c8c08c2e66fef7a69fb59914ecec
DianVK <diankinanev@abv.bg>
2022-12-28 12:19:32 +0200

    resolution

--- Advanced/Functions Advanced - Lab/cheese_showcase..py
+++ Advanced/Functions Advanced - Lab/cheese_showcase..py
@@ -1,30 +1,23 @@
 def sorting_cheeses(**kwargs):
-    result = ""
-    for cheese, quantities in sorted(kwargs.items(), key=lambda x: (-len(x[1]), x[0])):
-        result += f"{cheese}\n" + "\n".join(str(x) for x in sorted(quantities, reverse=True)) + "\n"
-        # result += "\n"
+    sorted_cheeses = sorted(kwargs.items(),key=lambda kvpt: (-len(kvpt[1]), kvpt[0]))
+    result = ''
+    for name,quantity in sorted_cheeses:
+        result += name + "\n"
+        quantity = sorted(quantity,reverse=True)
+        result += "\n".join([str(el) for el in quantity]) + "\n"
     return result
 
+# def sorting_cheeses(**kwargs):
+#     sorted_cheeses = sorted(kwargs.items(),key=lambda kvpt: (-len(kvpt[1]), kvpt[0]))
+#     for name,quantity in sorted_cheeses:
+#         print(name,end="\n")
+#         quantity = sorted(quantity,reverse=True)
+#         print("\n".join([str(el) for el in quantity]) + "\n")
 
-'''Camembert
-500
-430
-105
-100
-100
-'''
 print(
     sorting_cheeses(
         Parmesan=[102, 120, 135],
         Camembert=[100, 100, 105, 500, 430],
         Mozzarella=[50, 125],
-    )
-)
-
-print(
-    sorting_cheeses(
-        Parmigiano=[165, 215],
-        Feta=[150, 515],
-        Brie=[150, 125]
     )
 )
