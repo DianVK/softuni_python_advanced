a7930b76a40460f53d3525a59c749cea0bb27ef2
DianVK <diankinanev@abv.bg>
2023-01-02 14:34:46 +0200

    initial commit

---
+++ Python Advanced - Exams/Python Advanced Exam - 22 October 2022/hourly_forecast.py
@@ -0,0 +1,45 @@
+def forecast(*args):
+    result = ""
+    forecast_all_citys = {
+        'Sunny':[],
+        'Cloudy':[],
+        'Rainy':[]
+    }
+    for items in args:
+        city = items[0]
+        weather = items[1]
+        if weather == "Sunny":
+            forecast_all_citys['Sunny'].append(city)
+        elif weather == "Cloudy":
+            forecast_all_citys['Cloudy'].append(city)
+        elif weather == "Rainy":
+            forecast_all_citys['Rainy'].append(city)
+    forecast_all_citys['Sunny'] = sorted(forecast_all_citys['Sunny'])
+    forecast_all_citys['Cloudy'] = sorted(forecast_all_citys['Cloudy'])
+    forecast_all_citys['Rainy'] = sorted(forecast_all_citys['Rainy'])
+    for x in forecast_all_citys['Sunny']:
+        result += f"{x} - Sunny\n"
+    for x in forecast_all_citys['Cloudy']:
+        result += f"{x} - Cloudy\n"
+    for x in forecast_all_citys['Rainy']:
+        result += f"{x} - Rainy\n"
+    return result
+
+
+print(forecast(
+    ("Beijing", "Sunny"),
+    ("Hong Kong", "Rainy"),
+    ("Tokyo", "Sunny"),
+    ("Sofia", "Cloudy"),
+    ("Peru", "Sunny"),
+    ("Florence", "Cloudy"),
+    ("Bourgas", "Sunny")))
+
+print(forecast(
+    ("Tokyo", "Rainy"),
+    ("Sofia", "Rainy")))
+
+print(forecast(
+    ("Sofia", "Sunny"),
+    ("London", "Cloudy"),
+    ("New York", "Sunny")))
