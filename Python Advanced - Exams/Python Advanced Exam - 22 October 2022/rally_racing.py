6333fc224aa1f593e6c27bc6eafa6848abc50709
DianVK <diankinanev@abv.bg>
2023-01-02 14:21:11 +0200

    initial commit

--- Python Advanced - Exams/Python Advanced Exam - 22 October 2022/rally_racing.py
+++ Python Advanced - Exams/Python Advanced Exam - 22 October 2022/rally_racing.py
@@ -1,15 +1,21 @@
 def racing(r, c):
-    global row, column,teleport
+    global row, column,teleport,passed_km
     if 0 <= r < matrix_size and 0 <= c < matrix_size:
-        current_matrix = matrix
         current_element = matrix[r][c]
-        if current_element == "F":
+        if current_element == ".":
+            passed_km += 10
+        elif current_element == "F":
+            passed_km += 10
             matrix[row][column] = "."
             matrix[r][c] = "C"
             print(f"Racing car {racing_number} finished the stage!")
-            print(*matrix, sep="\n")
+            print(f"Distance covered {passed_km} km.")
+            for x in range(len(matrix)):
+                current_list = matrix[x]
+                print(*current_list, sep="")
             exit()
         elif current_element == "T":
+            passed_km += 30
             if r == tunel_entrance[0] and c == tunel_entrance[1]:
                 matrix[row][column] = "."
                 row = tunel_exit[0]
@@ -79,4 +85,56 @@ while command != "End":
     command = input()
 
 print(f"Racing car {racing_number} DNF.")
-print(*matrix, sep="\n")
+print(f"Distance covered {passed_km} km.")
+for x in range(len(matrix)):
+    current_list = matrix[x]
+    print(*current_list,sep="")
+
+################################################
+# size_matrix = int(input())
+# car_number = input()
+# matrix = []
+# total_km_passed = 0
+# car_coordinates = [0, 0]
+#
+# for _ in range(size_matrix):
+#     matrix.append(input().split())
+#
+#
+# def find_end_of_dune(mx):
+#     for row in range(len(mx)):
+#         for col in range(len(mx[row])):
+#             if mx[row][col] == "T":
+#                 return row, col
+#
+#
+# while True:
+#     command = input()
+#     if command == 'End':
+#         matrix[car_coordinates[0]][car_coordinates[1]] = "C"
+#         print(f"Racing car {car_number} DNF.")
+#         break
+#     elif command == "left":
+#         car_coordinates[1] -= 1
+#     elif command == "right":
+#         car_coordinates[1] += 1
+#     elif command == "up":
+#         car_coordinates[0] -= 1
+#     elif command == "down":
+#         car_coordinates[0] += 1
+#     if matrix[car_coordinates[0]][car_coordinates[1]] == "F":
+#         total_km_passed += 10
+#         matrix[car_coordinates[0]][car_coordinates[1]] = "C"
+#         print(f"Racing car {car_number} finished the stage!")
+#         break
+#     elif matrix[car_coordinates[0]][car_coordinates[1]] == "T":
+#         total_km_passed += 30
+#         matrix[car_coordinates[0]][car_coordinates[1]] = "."
+#         car_coordinates[0], car_coordinates[1] = find_end_of_dune(matrix)
+#         matrix[car_coordinates[0]][car_coordinates[1]] = "."
+#     else:
+#         total_km_passed += 10
+#
+# print(f"Distance covered {total_km_passed} km.")
+# for row in range(len(matrix)):
+#     print("".join(matrix[row]))
