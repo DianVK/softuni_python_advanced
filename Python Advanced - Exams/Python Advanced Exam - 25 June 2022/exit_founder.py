10989939bc4b9f66232b9f4381048ecfb62bdeba
DianVK <diankinanev@abv.bg>
2023-01-03 10:22:23 +0200

    initial commit

--- Python Advanced - Exams/Python Advanced Exam - 25 June 2022/exit_founder.py
+++ Python Advanced - Exams/Python Advanced Exam - 25 June 2022/exit_founder.py
@@ -1,7 +1,32 @@
-def check_location(r,c):
+def check_location(r, c):
+    global exit_found, winner, wall, player_on_wall
+
+    if player_on_wall != "" and player_on_wall == current_player:
+        player_on_wall = "free"
+        return
+
     if 0 <= r < matrix_size and 0 <= c < matrix_size:
         current_symbol = matrix[r][c]
-        return True
+
+        if current_symbol == "E":
+            exit_found = True
+            print(f"{current_player} found the Exit and wins the game!")
+            return
+
+        elif current_symbol == "T":
+            if current_player == first_name:
+                winner = second_name
+            elif current_player == second_name:
+                winner = first_name
+            exit_found = True
+            print(f"{current_player} is out of the game! The winner is {winner}.")
+            return
+
+        elif current_symbol == "W":
+            wall = True
+            print(f"{current_player} hits a wall and needs to rest.")
+            player_on_wall = current_player
+            return
 
 
 first_name, second_name = input().split(", ")
@@ -9,17 +34,27 @@ matrix_size = 6
 matrix = [[x for x in input().split()] for _ in range(matrix_size)]
 exit_found = False
 
-first_location = (0,0)
-second_location = (0,0)
-
 first = False
 second = False
+wall = False
+player_on_wall = ""
+current_player = ""
+
 while not exit_found:
     location = input()
-    row = location[0]
-    column = location[1]
+    row = int(location[1])
+    column = int(location[4])
     if not first and not second:
         first = True
-        check_location(row,column)
-
-
+        current_player = first_name
+        check_location(row, column)
+    elif first:
+        first = False
+        second = True
+        current_player = second_name
+        check_location(row, column)
+    elif second:
+        first = True
+        second = False
+        current_player = first_name
+        check_location(row, column)
