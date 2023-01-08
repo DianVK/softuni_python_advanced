3cd927bccc3fdc93b16406fb1d271b6068747c44
DianVK <diankinanev@abv.bg>
2023-01-08 18:51:35 +0200

    Initial commit

---
+++ Python OOP - Exercises/Classes and Objects - Exercise/library/registration.py
@@ -0,0 +1,28 @@
+from user import User
+from library import Library
+
+
+class Registration():
+    def add_user(self, user: User, library: Library):
+        if user not in library.user_records:
+            library.user_records.append(user)
+        else:
+            return f"User with id = {user.user_id} already registered in the library!"
+
+
+    def remove_user(self, user: User, library: Library):
+        if user in library.user_records:
+            library.user_records.remove(user)
+        else:
+            return f"We could not find such user to remove!"
+
+    def change_username(self, user_id: int, new_username: str, library: Library):
+        for pos, show in enumerate(library.user_records):
+            if show.user_id == user_id:
+                if show.username != new_username:
+                    if show.username in library.rented_books:
+                        library.rented_books[new_username] = library.rented_books.pop(show.username)
+                    show.username = new_username
+                    return f"Username successfully changed to: {new_username} for user id: {user_id}"
+                return f"Please check again the provided username - it should be different than the username used so far!"
+        return f"There is no user with id = {user_id}!"
