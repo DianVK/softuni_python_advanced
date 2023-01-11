c7d14de9252c519eeaeaaeb6d6b9516f8c130dfb
DianVK <diankinanev@abv.bg>
2023-01-11 17:07:09 +0200

    Initial Commit

---
+++ Python OOP - Exercises/Static and Class Methods - Lab/calculator.py
@@ -0,0 +1,37 @@
+class Calculator():
+    @staticmethod
+    def add(*args):
+        return sum(args)
+
+    @staticmethod
+    def multiply(*args):
+        result = 0
+        for num in args:
+            if result == 0:
+                result += num
+            else:
+                result *= num
+        return result
+
+    @staticmethod
+    def divide(*args):
+        result = 0
+        for num in args:
+            if result == 0:
+                result += num
+            else:
+                result /= num
+        return result
+
+    @staticmethod
+    def subtract(*args):
+        result = args[0]
+        for x in args[1:]:
+            result -= x
+        return result
+
+
+print(Calculator.add(5, 10, 4))
+print(Calculator.multiply(1, 2, 3, 5))
+print(Calculator.divide(100, 2))
+print(Calculator.subtract(90, 20, -50, 43, 7))
