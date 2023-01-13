11a4e7c1ef206a2e010fd8a49ddb9c33fe52b44c
DianVK <diankinanev@abv.bg>
2023-01-13 16:48:57 +0200

    Initial Commit

---
+++ Python OOP - Exercises/Polymorphism and Abstraction - Lab/playing.py
@@ -0,0 +1,18 @@
+class Guitar:
+    def play(self):
+        return "Playing the guitar"
+
+
+class Children:
+    def play(self):
+        return "Children are playing"
+
+
+def start_playing(obj):
+    return obj.play()
+
+
+guitar = Guitar()
+children = Children()
+print(start_playing(guitar))
+print(start_playing(children))
