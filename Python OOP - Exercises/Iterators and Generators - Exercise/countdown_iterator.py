d387e602d1147eeb9bc2bd30fd37bcf074954bd6
DianVK <diankinanev@abv.bg>
2023-01-22 15:25:47 +0200

    Initial commit

---
+++ Python OOP - Exercises/Iterators and Generators - Exercise/countdown_iterator.py
@@ -0,0 +1,17 @@
+class countdown_iterator:
+    def __init__(self,number:int):
+        self.number = number + 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.number <= 0:
+            raise StopIteration
+
+        self.number -= 1
+        return self.number
+
+iterator = countdown_iterator(10)
+for item in iterator:
+    print(item, end=" ")
