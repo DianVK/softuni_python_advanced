ad9f547ecc9842864ada9416f8ac84fc59cc23d5
DianVK <diankinanev@abv.bg>
2023-01-22 11:40:40 +0200

    Initial commit

--- Python OOP - Exercises/Iterators and Generators - Lab/squares.py
+++ Python OOP - Exercises/Iterators and Generators - Lab/squares.py
@@ -1,8 +1,22 @@
-def squares(num: int):
-    first_num = 1
-    while first_num <= num:
-        yield first_num ** 2
-        first_num += 1
+class squares:
+    def __init__(self, end: int):
+        self.start = 0
+        self.end = end
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.start >= self.end:
+            raise StopIteration
+        self.start += 1
+        return self.start ** 2
 
 
 print(list(squares(5)))
+
+# def squares(num: int):
+#     first_num = 1
+#     while first_num <= num:
+#         yield first_num ** 2
+#         first_num += 1
