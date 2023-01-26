10bc382cdbc1726e598edcc17c0f651950c49e74
DianVK <diankinanev@abv.bg>
2023-01-26 15:27:04 +0200

    Initial commit

---
+++ Python OOP - Exercises/Decorators - Exercise/even_parameters.py
@@ -0,0 +1,32 @@
+def even_parameters(func):
+    def wrapper(*args):
+        for num in args:
+
+            if isinstance(num, int) and num % 2 == 0:
+                continue
+            return "Please use only even numbers!"
+
+        return func(*args)
+
+    return wrapper
+
+
+@even_parameters
+def multiply(*nums):
+    result = 1
+    for num in nums:
+        result *= num
+    return result
+
+
+print(multiply(2, 4, 6, 8))
+print(multiply(2, 4, 9, 8))
+
+
+@even_parameters
+def add(a, b):
+    return a + b
+
+
+print(add(2, 4))
+print(add("Peter", 1))
