1edc3f580b474e6bff2957aaa7389e1ddbeb35ca
DianVK <diankinanev@abv.bg>
2023-01-26 16:26:27 +0200

    Initial commit

--- Python OOP - Exercises/Decorators - Exercise/type_check.py
+++ Python OOP - Exercises/Decorators - Exercise/type_check.py
@@ -17,3 +17,10 @@ def times2(num):
 
 print(times2(2))
 print(times2('Not A Number'))
+
+@type_check(str)
+def first_letter(word):
+    return word[0]
+
+print(first_letter('Hello World'))
+print(first_letter(['Not', 'A', 'String']))
