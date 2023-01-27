84c6a55b2bb8257f254a99de52ff43673e18a646
DianVK <diankinanev@abv.bg>
2023-01-27 10:44:51 +0200

    Initial commit

---
+++ Python OOP - Exercises/Decorators - Exercise/html_tags.py
@@ -0,0 +1,20 @@
+def tags(tag):
+    def decorator(func):
+        def wrapper(*args):
+            return f"<{tag}>{func(*args)}</{tag}>"
+        return wrapper
+
+    return decorator
+
+
+@tags('h1')
+@tags('p')
+def join_strings(*args):
+    return "".join(args)
+print(join_strings("Hello", " you!"))
+
+
+@tags('h1')
+def to_upper(text):
+    return text.upper()
+print(to_upper('hello'))
