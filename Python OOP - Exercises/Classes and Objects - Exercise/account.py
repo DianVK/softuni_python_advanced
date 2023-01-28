1c35da53ad773b385c7f100afe3dc7b19ab59d1f
DianVK <diankinanev@abv.bg>
2023-01-28 10:30:02 +0200

    Re-solving

--- Python OOP - Exercises/Classes and Objects - Exercise/account.py
+++ Python OOP - Exercises/Classes and Objects - Exercise/account.py
@@ -1,4 +1,4 @@
-class Account():
+class Account:
     def __init__(self, id: int, name: str, balance=0):
         self.id = id
         self.name = name
@@ -8,24 +8,27 @@ class Account():
         self.balance += amount
         return self.balance
 
-    def debit(self, amount):
-        if self.balance >= amount:
+    def debit(self, amount: int):
+        if amount <= self.balance:
             self.balance -= amount
             return self.balance
-        else:
-            return "Amount exceeded balance"
+        return "Amount exceeded balance"
 
     def info(self):
         return f"User {self.name} with account {self.id} has {self.balance} balance"
 
 
-account = Account(5411256, "Peter")
-print(account.debit(500))
-print(account.credit(1000))
-print(account.debit(500))
-print(account.info())
+ac = Account(1,"Dian",100)
+print(ac.credit(1000))
+print(ac.info())
 
 account = Account(1234, "George", 1000)
 print(account.credit(500))
 print(account.debit(1500))
+print(account.info())
+
+account = Account(5411256, "Peter")
+print(account.debit(500))
+print(account.credit(1000))
+print(account.debit(500))
 print(account.info())
