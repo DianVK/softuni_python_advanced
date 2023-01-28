0cdf4e475090939bfb74ad19d634aadaffbe8285
DianVK <diankinanev@abv.bg>
2023-01-28 09:27:39 +0200

    Re-solving

--- Python OOP - Exercises/Classes and Objects - Lab/smartphone.py
+++ Python OOP - Exercises/Classes and Objects - Lab/smartphone.py
@@ -1,33 +1,33 @@
-class Smartphone():
-    def __init__(self, memory: int):
+class Smartphone:
+    def __init__(self,memory:int):
         self.memory = memory
-        self.apps = []
+        self.apps = list()
         self.is_on = False
 
     def power(self):
-        if self.is_on == False:
+        if not self.is_on:
             self.is_on = True
-        elif self.is_on:
-            self.is_on = False
+            return self.is_on
+        self.is_on = False
+        return self.is_on
 
-    def install(self, app, app_memory):
-        if self.is_on and app_memory <= self.memory:
-            self.apps.append(app)
+    def install(self,app,app_memory):
+        if app_memory <= self.memory and self.is_on:
             self.memory -= app_memory
+            self.apps.append(app)
             return f"Installing {app}"
-        elif app_memory > self.memory:
-            return f"Not enough memory to install {app}"
-        elif self.is_on == False and app_memory <= self.memory:
+        elif app_memory <= self.memory and not self.is_on:
             return f"Turn on your phone to install {app}"
+        else:
+            return f"Not enough memory to install {app}"
 
     def status(self):
         return f"Total apps: {len(self.apps)}. Memory left: {self.memory}"
 
 
-smartphone = Smartphone(100)
-print(smartphone.install("Facebook", 60))
-smartphone.power()
-print(smartphone.install("Facebook", 60))
-print(smartphone.install("Messenger", 20))
-print(smartphone.install("Instagram", 40))
-print(smartphone.status())
+smp = Smartphone(500)
+print(smp.install("Facebook",300))
+print(smp.status())
+print(smp.power())
+print(smp.install("Messenger",200))
+print(smp.status())
