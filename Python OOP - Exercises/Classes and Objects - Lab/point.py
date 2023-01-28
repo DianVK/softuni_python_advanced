7d08b8fc3cfa0ecc47b16bd4fbbc4bcd729b52fa
DianVK <diankinanev@abv.bg>
2023-01-28 09:02:17 +0200

    Re-solving

--- Python OOP - Exercises/Classes and Objects - Lab/point.py
+++ Python OOP - Exercises/Classes and Objects - Lab/point.py
@@ -1,20 +1,22 @@
-class Point():
+class Point:
     def __init__(self, x: int, y: int):
         self.x = x
         self.y = y
 
     def set_x(self, new_x):
         self.x = new_x
+        return self.x
 
     def set_y(self, new_y):
         self.y = new_y
+        return self.y
 
     def __str__(self):
-        return f"The point has coordinates ({self.x},{self.y}) "
+        return f"The point has coordinates ({self.x},{self.y})"
 
 
-p = Point(2, 4)
+p = Point(5, 4)
 print(p)
-p.set_x(3)
-p.set_y(5)
+p.set_x(10)
+p.set_y(11)
 print(p)
