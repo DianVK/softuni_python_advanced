fd0328fa7455e8e9fa61d9134ed0e4c3653eb68f
DianVK <diankinanev@abv.bg>
2023-01-28 10:13:05 +0200

    Re-solving

--- Python OOP - Exercises/Classes and Objects - Exercise/vet.py
+++ Python OOP - Exercises/Classes and Objects - Exercise/vet.py
@@ -1,29 +1,28 @@
 class Vet:
-    animals = []
+    animals = list()
     space = 5
 
-    def __init__(self, name):
+    def __init__(self, name: str):
         self.name = name
-        self.animals = []
+        self.animals = list()
 
     def register_animal(self, animal_name):
-        if Vet.space > 0:
-            Vet.space -= 1
-            Vet.animals.append(animal_name)
+        if len(self.animals) < Vet.space:
             self.animals.append(animal_name)
+            Vet.animals.append(animal_name)
             return f"{animal_name} registered in the clinic"
         return "Not enough space"
 
     def unregister_animal(self, animal_name):
-        if animal_name in self.animals and animal_name in Vet.animals:
+        if animal_name in self.animals:
+
             self.animals.remove(animal_name)
             Vet.animals.remove(animal_name)
-            Vet.space += 1
             return f"{animal_name} unregistered successfully"
         return f"{animal_name} not in the clinic"
 
     def info(self):
-        return f"{self.name} has {len(self.animals)} animals. {Vet.space} space left in clinic"
+        return f"{self.name} has {len(self.animals)} animals. {5 - len(Vet.animals)} space left in clinic"
 
 
 peter = Vet("Peter")
