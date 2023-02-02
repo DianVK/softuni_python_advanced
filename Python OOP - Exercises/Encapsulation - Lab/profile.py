0416129a7dc7d95320b253319f5b1573fb3f17b4
DianVK <diankinanev@abv.bg>
2023-02-02 18:22:59 +0200

    Re-solving

--- Python OOP - Exercises/Encapsulation - Lab/profile.py
+++ Python OOP - Exercises/Encapsulation - Lab/profile.py
@@ -1,4 +1,5 @@
-class Profile():
+class Profile:
+
     def __init__(self, username: str, password):
         self.username = username
         self.password = password
@@ -9,7 +10,7 @@ class Profile():
 
     @username.setter
     def username(self, value):
-        if 5 > len(value) or len(value) > 15:
+        if not 5 <= len(value) <= 15:
             raise ValueError("The username must be between 5 and 15 characters.")
         self.__username = value
         return
@@ -21,29 +22,21 @@ class Profile():
 
     @password.setter
     def password(self, value):
-        if len(value) >= 8:
-            one_letter = False
-            one_num = False
-            for x in value:
-                if x.isdigit():
-                    one_num = True
-                elif x.isalpha():
-                    if x.isupper():
-                        one_letter = True
-            if one_num and one_letter:
-                self.__password = value
-                return
-            else:
-                raise ValueError(
-                    "The password must be 8 or more characters with at least 1 digit and 1 uppercase letter.")
-        else:
+        upper = any(x.isupper() for x in value)
+        digit = any(x.isdigit() for x in value)
+        if len(value) < 8 or not upper or not digit:
             raise ValueError("The password must be 8 or more characters with at least 1 digit and 1 uppercase letter.")
+
+        self.__password = value
+        return
 
     def __str__(self):
         return f'You have a profile with username: "{self.__username}" and password: {"*" * len(self.__password)}'
 
 
-# profile_with_invalid_password = Profile('My_username', 'My-password')
-# profile_with_invalid_username = Profile('Too_long_username', 'Any')
+#profile_with_invalid_password = Profile('My_username', 'My-password')
+
+#profile_with_invalid_username = Profile('Too_long_username', 'Any')
+
 correct_profile = Profile("Username", "Passw0rd")
 print(correct_profile)
