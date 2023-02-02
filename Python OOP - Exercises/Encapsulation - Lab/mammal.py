15f8379adaee9e66ba88fadc89f654f90bef3d3d
DianVK <diankinanev@abv.bg>
2023-02-02 17:05:13 +0200

    Re-solving

--- Python OOP - Exercises/Encapsulation - Lab/mammal.py
+++ Python OOP - Exercises/Encapsulation - Lab/mammal.py
@@ -1,19 +1,21 @@
-class Mammal():
+class Mammal:
     __kingdom = "animals"
 
-    def __init__(self, name: str, type, sound):
+    def __init__(self, name, type, sound):
         self.name = name
         self.type = type
         self.sound = sound
 
     def make_sound(self):
-        return f"{self.name} makes {self.sound}"
+        result = f"{self.name} makes {self.sound}"
+        return result
 
     def get_kingdom(self):
         return Mammal.__kingdom
 
     def info(self):
-        return f"{self.name} is of type {self.type}"
+        result = f"{self.name} is of type {self.type}"
+        return result
 
 
 mammal = Mammal("Dog", "Domestic", "Bark")
