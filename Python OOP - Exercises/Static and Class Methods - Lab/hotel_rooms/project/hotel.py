1095b6d0ac287c2292cbe2af798fbb58c62df72e
DianVK <diankinanev@abv.bg>
2023-02-11 14:40:22 +0200

    Re-solving

--- Python OOP - Exercises/Static and Class Methods - Lab/hotel_rooms/project/hotel.py
+++ Python OOP - Exercises/Static and Class Methods - Lab/hotel_rooms/project/hotel.py
@@ -1,25 +1,29 @@
-from room import Room
+from project.room import Room
 
 
 class Hotel:
-
-    def __init__(self, name):
+    def __init__(self, name: str):
         self.name = name
-        self.rooms = []
+        self.rooms = list()
         self.guests = 0
 
-    @staticmethod
-    def from_stars(stars_count: int):
-        return Hotel(f"{stars_count} stars Hotel")
+    @classmethod
+    def from_stars(cls,stars_count:int):
+        new_ins = f"{stars_count} stars Hotel"
+        return cls(new_ins)
 
     def add_room(self, room: Room):
         self.rooms.append(room)
+        return
 
-    def take_room(self, room_number, people):
+    def take_room(self,room_number, people):
         [room.take_room(people) for room in self.rooms if room.number == room_number]
+        # for room in self.rooms:
+        #     if room == room_number:
+        #         room.take_room(people)
 
-    def free_room(self, room_number):
-        [room.free_room() for room in self.rooms if room.number == room_number]
+    def free_room(self,room_number):
+        [room.free_room() for room in self.rooms if room == room_number]
 
     def status(self):
         self.guests = 0
@@ -30,25 +34,7 @@ class Hotel:
                 self.guests += x.guests
             else:
                 free_rooms.append(str(x.number))
-        output = [f"Hotel {self.name} has {self.guests} total guests",
-                  f"Free rooms: {', '.join(free_rooms)}",
-                  f"Taken rooms: {', '.join(taken_rooms)}"]
 
-        return "\n".join(output)
+        output = f"Hotel {self.name} has {self.guests} total guests\nFree rooms: {', '.join(free_rooms)}\nTaken rooms: {', '.join(taken_rooms)}"
+        return output
 
-hotel = Hotel.from_stars(5)
-
-first_room = Room(1, 3)
-second_room = Room(2, 2)
-third_room = Room(3, 1)
-
-hotel.add_room(first_room)
-hotel.add_room(second_room)
-hotel.add_room(third_room)
-
-hotel.take_room(1, 4)
-hotel.take_room(1, 2)
-hotel.take_room(3, 1)
-hotel.take_room(3, 1)
-
-print(hotel.status())
--- Python OOP - Exercises/Static and Class Methods - Lab/hotel_rooms/project/room.py
+++ Python OOP - Exercises/Static and Class Methods - Lab/hotel_rooms/project/room.py
@@ -1,20 +1,23 @@
 class Room:
-    def __init__(self, number, capacity):
-        self.capacity = capacity
+
+    def __init__(self, number: int, capacity: int):
         self.number = number
+        self.capacity = capacity
         self.guests = 0
         self.is_taken = False
 
-    def take_room(self, people):
-        if not self.is_taken and self.capacity >= people:
+    def take_room(self,people):
+        if not self.is_taken and self.capacity + self.guests > people:
             self.is_taken = True
-            self.guests = people
-        else:
-            return f"Room number {self.number} cannot be taken"
+            self.guests += people
+            return
+
+        return f"Room number {self.number} cannot be taken"
 
     def free_room(self):
         if self.is_taken:
-            self.guests = 0
             self.is_taken = False
-        else:
-            return f"Room number {self.number} is not taken"
+            return
+        return f"Room number {self.number} is not taken"
+
+
