f2e5f6ff98d494fe982355fae3e50c7ba292dd85
DianVK <diankinanev@abv.bg>
2023-02-12 12:41:04 +0200

    Re-solving

--- Python OOP - Exercises/Polymorphism and Abstraction - Lab/image_area.py
+++ Python OOP - Exercises/Polymorphism and Abstraction - Lab/image_area.py
@@ -1,4 +1,5 @@
 class ImageArea:
+
     def __init__(self, width: int, height: int):
         self.width = width
         self.height = height
@@ -30,15 +31,3 @@ a2 = ImageArea(35, 2)
 a3 = ImageArea(8, 9)
 print(a1 == a2)
 print(a1 != a3)
-
-a1 = ImageArea(7, 10)
-a2 = ImageArea(35, 2)
-a3 = ImageArea(8, 9)
-print(a1 != a2)
-print(a1 >= a3)
-
-a1 = ImageArea(7, 10)
-a2 = ImageArea(35, 2)
-a3 = ImageArea(8, 9)
-print(a1 <= a2)
-print(a1 < a3)
