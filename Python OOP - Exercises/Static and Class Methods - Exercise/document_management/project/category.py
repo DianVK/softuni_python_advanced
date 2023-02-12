cb2e5642af9736efed76b4195ac39e4732fa95b9
DianVK <diankinanev@abv.bg>
2023-02-12 10:48:09 +0200

    Re-solving

--- Python OOP - Exercises/Static and Class Methods - Exercise/document_management/project/category.py
+++ Python OOP - Exercises/Static and Class Methods - Exercise/document_management/project/category.py
@@ -1,5 +1,6 @@
 class Category:
-    def __init__(self, id: int, name: str):
+
+    def __init__(self,id: int, name: str):
         self.id = id
         self.name = name
 
--- Python OOP - Exercises/Static and Class Methods - Exercise/document_management/project/document.py
+++ Python OOP - Exercises/Static and Class Methods - Exercise/document_management/project/document.py
@@ -3,28 +3,32 @@ from project.topic import Topic
 
 
 class Document:
-    def __init__(self, id_number: int, category_id: int, topic_id: int, file_name: str):
-        self.id = id_number
+
+    def __init__(self,id: int, category_id: int, topic_id: int, file_name: str):
+        self.id = id
         self.category_id = category_id
         self.topic_id = topic_id
         self.file_name = file_name
         self.tags = []
 
     @classmethod
-    def from_instances(cls, id_number: int, category: Category, topic: Topic, file_name: str):
-        return cls(id_number, category.id, topic.id, file_name)
+    def from_instances(cls, document_id: int, category: Category, topic: Topic, file_name: str):
+
+        return cls(document_id, category.id, topic.id, file_name)
 
     def add_tag(self, tag_content: str):
         if tag_content not in self.tags:
             self.tags.append(tag_content)
+            return
 
     def remove_tag(self, tag_content: str):
         if tag_content in self.tags:
             self.tags.remove(tag_content)
+            return
 
     def edit(self, file_name: str):
         self.file_name = file_name
 
     def __repr__(self):
-        return f"Document {self.id}: {self.file_name}; category {self.category_id}, topic {self.topic_id}," \
-               f" tags: {', '.join(self.tags)}"
+        return f"Document {self.id}: {self.file_name}; category {self.category_id}," \
+               f" topic {self.topic_id}, tags: {', '.join(self.tags)}"
