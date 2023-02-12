42331af97e82d0050699070f3bdf0ca2330beb70
DianVK <diankinanev@abv.bg>
2023-02-12 15:57:18 +0200

    Re-solving

--- Python OOP - Exercises/Polymorphism and Abstraction - Exercise/vehicle.py
+++ Python OOP - Exercises/Polymorphism and Abstraction - Exercise/vehicle.py
@@ -2,53 +2,60 @@ from abc import ABC, abstractmethod
 
 
 class Vehicle(ABC):
-    def __init__(self, fuel_quantity: float, fuel_consumption: float):
-        self.fuel_quantity = fuel_quantity
-        self.fuel_consumption = fuel_consumption
 
     @abstractmethod
-    def drive(self, distance: float):
+    def drive(self, distance):
         pass
 
     @abstractmethod
-    def refuel(self, fuel: float):
+    def refuel(self, fuel):
         pass
 
 
 class Car(Vehicle):
-    CONDITIONER_ON = 0.9
+    CONSUMPTION = 0.9
 
-    def drive(self, distance: float):
-        consumption = (self.fuel_consumption + Car.CONDITIONER_ON) * distance
+    def __init__(self, fuel_quantity, fuel_consumption):
+        self.fuel_quantity = fuel_quantity
+        self.fuel_consumption = fuel_consumption
 
-        if self.fuel_quantity - consumption >= 0:
-            self.fuel_quantity -= consumption
+    def drive(self,distance):
+        needed_fuel = (self.fuel_consumption + Car.CONSUMPTION) * distance
+        if needed_fuel <= self.fuel_quantity:
+            self.fuel_quantity -= needed_fuel
+            return
 
-    def refuel(self, fuel: float):
+    def refuel(self,fuel):
         self.fuel_quantity += fuel
+        return
 
 
 class Truck(Vehicle):
-    CONDITIONER_ON = 1.6
+    CONSUMPTION = 1.6
 
-    def drive(self, distance: float):
-        consumption = (self.fuel_consumption + Truck.CONDITIONER_ON) * distance
+    def __init__(self, fuel_quantity, fuel_consumption):
+        self.fuel_quantity = fuel_quantity
+        self.fuel_consumption = fuel_consumption
 
-        if self.fuel_quantity - consumption >= 0:
-            self.fuel_quantity -= consumption
+    def drive(self,distance):
+        needed_fuel = (self.fuel_consumption + Truck.CONSUMPTION) * distance
+        if needed_fuel <= self.fuel_quantity:
+            self.fuel_quantity -= needed_fuel
+            return
 
-    def refuel(self, fuel: float):
+    def refuel(self,fuel):
         self.fuel_quantity += fuel * 0.95
+        return
 
-
-car = Car(20, 5)
-car.drive(3)
-print(car.fuel_quantity)
-car.refuel(10)
-print(car.fuel_quantity)
 
 truck = Truck(100, 15)
 truck.drive(5)
 print(truck.fuel_quantity)
 truck.refuel(50)
 print(truck.fuel_quantity)
+
+car = Car(20, 5)
+car.drive(3)
+print(car.fuel_quantity)
+car.refuel(10)
+print(car.fuel_quantity)
