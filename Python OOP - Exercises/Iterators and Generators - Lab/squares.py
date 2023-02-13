ca2d32b5a7d99c4eca98914aca7c329497b21860
DianVK <diankinanev@abv.bg>
2023-02-13 19:00:07 +0200

    Re-solving

--- Python OOP - Exercises/Iterators and Generators - Lab/squares.py
+++ Python OOP - Exercises/Iterators and Generators - Lab/squares.py
@@ -1,22 +1,38 @@
-class squares:
-    def __init__(self, end: int):
-        self.start = 0
-        self.end = end
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        if self.start >= self.end:
-            raise StopIteration
-        self.start += 1
-        return self.start ** 2
+def squares(num: int):
+    first_num = 1
+    while first_num <= num:
+        yield first_num ** 2
+        first_num += 1
 
 
 print(list(squares(5)))
 
-# def squares(num: int):
-#     first_num = 1
-#     while first_num <= num:
-#         yield first_num ** 2
-#         first_num += 1
+# class squares:
+#
+#     def __init__(self, n):
+#         self.n = n
+#         self.number_counter = 0
+#
+#     def __iter__(self):
+#         return self
+#
+#     def __next__(self):
+#         if self.number_counter < self.n:
+#             self.number_counter += 1
+#             return self.number_counter ** 2
+#
+#         raise StopIteration
+
+# class squares:
+#     def __init__(self, end: int):
+#         self.start = 0
+#         self.end = end
+#
+#     def __iter__(self):
+#         return self
+#
+#     def __next__(self):
+#         if self.start >= self.end:
+#             raise StopIteration
+#         self.start += 1
+#         return self.start ** 2
