d7c7feb7d6b10e827ec582b2262ad702790b59c2
DianVK <diankinanev@abv.bg>
2023-02-14 10:36:39 +0200

    Re-solving

--- Python OOP - Exercises/Iterators and Generators - Exercise/countdown_iterator.py
+++ Python OOP - Exercises/Iterators and Generators - Exercise/countdown_iterator.py
@@ -1,18 +1,40 @@
-class countdown_iterator:
-    def __init__(self, number: int):
-        self.number = number + 1
+class countdown_iterator():
+
+    def __init__(self, count: int):
+        self.count = count
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        if self.number <= 0:
+        if self.count < 0:
             raise StopIteration
 
-        self.number -= 1
-        return self.number
+        self.count -= 1
+        return self.count + 1
 
 
 iterator = countdown_iterator(10)
 for item in iterator:
     print(item, end=" ")
+
+print("\n".strip())
+
+iterator = countdown_iterator(0)
+for item in iterator:
+    print(item, end=" ")
+
+
+# class countdown_iterator:
+#     def __init__(self, number: int):
+#         self.number = number + 1
+#
+#     def __iter__(self):
+#         return self
+#
+#     def __next__(self):
+#         if self.number <= 0:
+#             raise StopIteration
+#
+#         self.number -= 1
+#         return self.number
