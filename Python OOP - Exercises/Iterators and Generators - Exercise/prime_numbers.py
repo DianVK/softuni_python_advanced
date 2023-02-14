f95078700c44743825ad5bbb2cdc3437bc348a85
DianVK <diankinanev@abv.bg>
2023-02-14 15:43:19 +0200

    Re-solving

--- Python OOP - Exercises/Iterators and Generators - Exercise/prime_numbers.py
+++ Python OOP - Exercises/Iterators and Generators - Exercise/prime_numbers.py
@@ -1,16 +1,19 @@
 def get_primes(*args):
-    prime_numbers = [
+
+    prime_nums = [
         2, 3, 5, 7, 11, 13, 17, 19,
         23, 29, 31, 37, 41, 43, 47, 53,
         59, 61, 67, 71, 73, 79, 83, 89,
         97, 101, 103, 107, 109, 113, 127, 131,
         137, 139, 149, 151, 157, 163, 167, 173,
-        179, 181, 191, 193, 197, 199, 211, 223
-    ]
+        179, 181, 191, 193, 197, 199, 211, 223]
+
     for x in args:
-        for y in x:
-            if y in prime_numbers:
-                yield y
+        for num in x:
+            if num in prime_nums:
+                yield num
 
 
-print(list(get_primes([2, 3, 5, 7, 11, 13, 17, 19, 23, 29])))
+print(list(get_primes([2, 4, 3, 5, 6, 9, 1, 0])))
+
+print(list(get_primes([-2, 0, 0, 1, 1, 0])))
