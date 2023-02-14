d5aa7fd604e60b4813e81542bea348d0a148fdb4
DianVK <diankinanev@abv.bg>
2023-02-14 09:54:45 +0200

    Re-solving

--- Python OOP - Exercises/Iterators and Generators - Lab/reverse_strings.py
+++ Python OOP - Exercises/Iterators and Generators - Lab/reverse_strings.py
@@ -1,9 +1,17 @@
-def reverse_text(text: str):
-    index = len(text) - 1
-    while index >= 0:
-        yield text[index]
-        index -= 1
+def reverse_text(text):
+    text = text[::-1]
+    for x in range(len(text)):
+        current_letter = text[x]
+        yield current_letter
 
 
 for char in reverse_text("step"):
     print(char, end='')
+
+
+# def reverse_text(text: str):
+#     index = len(text) - 1
+#     while index >= 0:
+#         yield text[index]
+#         index -= 1
+
