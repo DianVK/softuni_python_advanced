450ccbd7f9c9f842e776e138958ee2e21bff2c78
DianVK <diankinanev@abv.bg>
2023-02-14 15:15:32 +0200

    Re-solving

--- Python OOP - Exercises/Iterators and Generators - Exercise/fibonacci_generator.py
+++ Python OOP - Exercises/Iterators and Generators - Exercise/fibonacci_generator.py
@@ -1,13 +1,25 @@
 def fibonacci():
-    n1, n2 = 0, 1
+    now_num = 1
+    old_num = 0
 
     while True:
-        yield n1
-
-        n1, n2 = n2, n1 + n2
+        yield old_num
+        old_num, now_num = now_num, old_num + now_num
 
 
 generator = fibonacci()
-
 for i in range(5):
     print(next(generator))
+
+# generator = fibonacci()
+# for i in range(1):
+#     print(next(generator))
+
+
+# def fibonacci():
+#     n1, n2 = 0, 1
+#
+#     while True:
+#         yield n1
+#
+#         n1, n2 = n2, n1 + n2
