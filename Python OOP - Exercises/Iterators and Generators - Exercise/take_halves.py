64c2d1fd5557c069d4d2ed797429008cfcc75df3
DianVK <diankinanev@abv.bg>
2023-02-14 11:48:55 +0200

    Re-solving

--- Python OOP - Exercises/Iterators and Generators - Exercise/take_halves.py
+++ Python OOP - Exercises/Iterators and Generators - Exercise/take_halves.py
@@ -1,9 +1,9 @@
 def solution():
+
     def integers():
         num = 1
         while True:
             yield num
-
             num += 1
 
     def halves():
@@ -18,5 +18,44 @@ def solution():
 
 take = solution()[0]
 halves = solution()[1]
-
 print(take(5, halves()))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def solution():
+#     def integers():
+#         num = 1
+#         while True:
+#             yield num
+#
+#             num += 1
+#
+#     def halves():
+#         for num in integers():
+#             yield num / 2
+#
+#     def take(n, seq):
+#         return [next(seq) for _ in range(n)]
+#
+#     return take, halves, integers
+#
+#
+# take = solution()[0]
+# halves = solution()[1]
+#
+# print(take(5, halves()))
