bcf3fdc1d7f380efcbcc2ccb96c81abda9ff750f
DianVK <diankinanev@abv.bg>
2023-02-14 15:49:32 +0200

    Re-solving

--- Python OOP - Exercises/Iterators and Generators - Exercise/possible_permutations.py
+++ Python OOP - Exercises/Iterators and Generators - Exercise/possible_permutations.py
@@ -1,9 +1,11 @@
 from itertools import permutations
 
 
-def possible_permutations(nums):
-    for x in permutations(nums):
+def possible_permutations(numbers):
+    for x in permutations(numbers):
         yield list(x)
 
 
 [print(n) for n in possible_permutations([1, 2, 3])]
+
+[print(n) for n in possible_permutations([1])]
