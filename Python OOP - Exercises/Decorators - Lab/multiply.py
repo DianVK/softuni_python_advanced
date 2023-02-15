fe5dc6e00df93b796a071bf044d568fc1cb80133
DianVK <diankinanev@abv.bg>
2023-02-15 12:38:33 +0200

    Re-solving

--- Python OOP - Exercises/Decorators - Lab/multiply.py
+++ Python OOP - Exercises/Decorators - Lab/multiply.py
@@ -1,9 +1,24 @@
 def multiply(times):
-
     def decorator(function):
-        def wrapper(*args):
-            result = function(*args)
-            return result * times
+        def wrapper(num_add_ten):
+            num = function(num_add_ten)
+            return num * times
+
         return wrapper
 
     return decorator
+
+
+@multiply(3)
+def add_ten(number):
+    return number + 10
+
+
+print(add_ten(3))
+
+
+# @multiply(5)
+# def add_ten(number):
+#     return number + 10
+#
+# print(add_ten(6))
