d5829d85d541b0a38b2d759a3fd6f7eb0682a421
DianVK <diankinanev@abv.bg>
2023-02-15 16:29:14 +0200

    Re-solving

--- Python OOP - Exercises/Decorators - Exercise/even_parameters.py
+++ Python OOP - Exercises/Decorators - Exercise/even_parameters.py
@@ -1,15 +1,23 @@
-def even_parameters(func):
+def even_parameters(function):
+
     def wrapper(*args):
-        for num in args:
+        for x in args:
 
-            if isinstance(num, int) and num % 2 == 0:
+            if isinstance(x, int) and x % 2 == 0:
                 continue
-            return "Please use only even numbers!"
 
-        return func(*args)
-
+            return f"Please use only even numbers!"
+        return function(*args)
     return wrapper
 
+
+@even_parameters
+def add(a, b):
+    return a + b
+
+
+print(add(2, 4))
+print(add("Peter", 1))
 
 @even_parameters
 def multiply(*nums):
@@ -23,10 +31,54 @@ print(multiply(2, 4, 6, 8))
 print(multiply(2, 4, 9, 8))
 
 
-@even_parameters
-def add(a, b):
-    return a + b
 
 
-print(add(2, 4))
-print(add("Peter", 1))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def even_parameters(func):
+#     def wrapper(*args):
+#         for num in args:
+#
+#             if isinstance(num, int) and num % 2 == 0:
+#                 continue
+#             return "Please use only even numbers!"
+#
+#         return func(*args)
+#
+#     return wrapper
+#
+#
+# @even_parameters
+# def multiply(*nums):
+#     result = 1
+#     for num in nums:
+#         result *= num
+#     return result
+#
+#
+# print(multiply(2, 4, 6, 8))
+# print(multiply(2, 4, 9, 8))
+#
+#
+# @even_parameters
+# def add(a, b):
+#     return a + b
+#
+#
+# print(add(2, 4))
+# print(add("Peter", 1))
