4b9847e81b4286fa3514025092018a3ba446f329
DianVK <diankinanev@abv.bg>
2023-02-15 12:31:08 +0200

    Re-solving

--- Python OOP - Exercises/Decorators - Lab/even_numbers.py
+++ Python OOP - Exercises/Decorators - Lab/even_numbers.py
@@ -1,15 +1,16 @@
 def even_numbers(function):
 
     def wrapper(numbers):
+
         numbers = [x for x in numbers if x % 2 == 0]
         return numbers
 
     return wrapper
 
 
-# @even_numbers
-# def get_numbers(numbers):
-#     return numbers
-#
-#
-# print(get_numbers([1, 2, 3, 4, 5]))
+@even_numbers
+def get_numbers(numbers):
+    return numbers
+
+
+print(get_numbers([1, 2, 3, 4, 5]))
