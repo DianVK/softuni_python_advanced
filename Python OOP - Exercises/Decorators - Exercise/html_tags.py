10feaf98350172aac5a2077d0d9228e966560d7d
DianVK <diankinanev@abv.bg>
2023-02-16 10:16:56 +0200

    Re-solving

--- Python OOP - Exercises/Decorators - Exercise/html_tags.py
+++ Python OOP - Exercises/Decorators - Exercise/html_tags.py
@@ -7,14 +7,24 @@ def tags(tag):
     return decorator
 
 
-@tags('h1')
 @tags('p')
 def join_strings(*args):
     return "".join(args)
+
+
 print(join_strings("Hello", " you!"))
 
 
 @tags('h1')
 def to_upper(text):
     return text.upper()
+
+
 print(to_upper('hello'))
+
+# def tags(type_text):
+#     def decorator(func):
+#         def wrapper(*args):
+#             return f"<{type_text}>{func(''.join(args))}</{type_text}>"
+#         return wrapper
+#     return decorator
